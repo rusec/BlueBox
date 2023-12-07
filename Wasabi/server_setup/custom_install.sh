8e696757cd611fa0f9cb7907befadfc636917bbc
mohamad khawam <khawammohamad99@gmail.com>
2023-12-07 08:44:20 -0500

    changed to localhost

--- Wasabi/server_setup/custom_install.sh
+++ Wasabi/server_setup/custom_install.sh
@@ -1289,7 +1289,7 @@ function common_checkSystem() {
 function indexer_initialize() {
 
     common_logger "Initializing Wazuh indexer cluster security settings."
-    eval "common_curl -XGET https://"${indexer_node_ips[pos]}":9200/ -uadmin:admin -k --max-time 120 --silent --output /dev/null"
+    eval "common_curl -XGET https://localhost:9200/ -uadmin:admin -k --max-time 120 --silent --output /dev/null"
     e_code="${PIPESTATUS[0]}"
 
     if [ "${e_code}" -ne "0" ]; then
@@ -1519,23 +1519,229 @@ function dashboard_initializeAIO() {
         exit 1
     fi
 }
+# function cert_convertCRLFtoLF() {
+#     if [[ ! -d "/tmp/wazuh-install-files" ]]; then
+#         mkdir "/tmp/wazuh-install-files"
+#     fi
+#     eval "chmod -R 755 /tmp/wazuh-install-files ${debug}"
+#     eval "tr -d '\015' < $1 > /tmp/wazuh-install-files/new_config.yml"
+#     eval "mv /tmp/wazuh-install-files/new_config.yml $1"
+# }
+# function cert_parseYaml() {
+
+#     local prefix=$2
+#     local separator=${3:-_}
+#     local indexfix
+#     # Detect awk flavor
+#     if awk --version 2>&1 | grep -q "GNU Awk" ; then
+#     # GNU Awk detected
+#     indexfix=-1
+#     elif awk -Wv 2>&1 | grep -q "mawk" ; then
+#     # mawk detected
+#     indexfix=0
+#     fi
+
+#     local s='[[:space:]]*' sm='[ \t]*' w='[a-zA-Z0-9_]*' fs=${fs:-$(echo @|tr @ '\034')} i=${i:-  }
+#     cat $1 2>/dev/null | \
+#     awk -F$fs "{multi=0; 
+#         if(match(\$0,/$sm\|$sm$/)){multi=1; sub(/$sm\|$sm$/,\"\");}
+#         if(match(\$0,/$sm>$sm$/)){multi=2; sub(/$sm>$sm$/,\"\");}
+#         while(multi>0){
+#             str=\$0; gsub(/^$sm/,\"\", str);
+#             indent=index(\$0,str);
+#             indentstr=substr(\$0, 0, indent+$indexfix) \"$i\";
+#             obuf=\$0;
+#             getline;
+#             while(index(\$0,indentstr)){
+#                 obuf=obuf substr(\$0, length(indentstr)+1);
+#                 if (multi==1){obuf=obuf \"\\\\n\";}
+#                 if (multi==2){
+#                     if(match(\$0,/^$sm$/))
+#                         obuf=obuf \"\\\\n\";
+#                         else obuf=obuf \" \";
+#                 }
+#                 getline;
+#             }
+#             sub(/$sm$/,\"\",obuf);
+#             print obuf;
+#             multi=0;
+#             if(match(\$0,/$sm\|$sm$/)){multi=1; sub(/$sm\|$sm$/,\"\");}
+#             if(match(\$0,/$sm>$sm$/)){multi=2; sub(/$sm>$sm$/,\"\");}
+#         }
+#     print}" | \
+#     sed  -e "s|^\($s\)?|\1-|" \
+#         -ne "s|^$s#.*||;s|$s#[^\"']*$||;s|^\([^\"'#]*\)#.*|\1|;t1;t;:1;s|^$s\$||;t2;p;:2;d" | \
+#     sed -ne "s|,$s\]$s\$|]|" \
+#         -e ":1;s|^\($s\)\($w\)$s:$s\(&$w\)\?$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1\2: \3[\4]\n\1$i- \5|;t1" \
+#         -e "s|^\($s\)\($w\)$s:$s\(&$w\)\?$s\[$s\(.*\)$s\]|\1\2: \3\n\1$i- \4|;" \
+#         -e ":2;s|^\($s\)-$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1- [\2]\n\1$i- \3|;t2" \
+#         -e "s|^\($s\)-$s\[$s\(.*\)$s\]|\1-\n\1$i- \2|;p" | \
+#     sed -ne "s|,$s}$s\$|}|" \
+#         -e ":1;s|^\($s\)-$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1- {\2}\n\1$i\3: \4|;t1" \
+#         -e "s|^\($s\)-$s{$s\(.*\)$s}|\1-\n\1$i\2|;" \
+#         -e ":2;s|^\($s\)\($w\)$s:$s\(&$w\)\?$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1\2: \3 {\4}\n\1$i\5: \6|;t2" \
+#         -e "s|^\($s\)\($w\)$s:$s\(&$w\)\?$s{$s\(.*\)$s}|\1\2: \3\n\1$i\4|;p" | \
+#     sed  -e "s|^\($s\)\($w\)$s:$s\(&$w\)\(.*\)|\1\2:\4\n\3|" \
+#         -e "s|^\($s\)-$s\(&$w\)\(.*\)|\1- \3\n\2|" | \
+#     sed -ne "s|^\($s\):|\1|" \
+#         -e "s|^\($s\)\(---\)\($s\)||" \
+#         -e "s|^\($s\)\(\.\.\.\)\($s\)||" \
+#         -e "s|^\($s\)-$s[\"']\(.*\)[\"']$s\$|\1$fs$fs\2|p;t" \
+#         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p;t" \
+#         -e "s|^\($s\)-$s\(.*\)$s\$|\1$fs$fs\2|" \
+#         -e "s|^\($s\)\($w\)$s:$s[\"']\?\(.*\)$s\$|\1$fs\2$fs\3|" \
+#         -e "s|^\($s\)[\"']\?\([^&][^$fs]\+\)[\"']$s\$|\1$fs$fs$fs\2|" \
+#         -e "s|^\($s\)[\"']\?\([^&][^$fs]\+\)$s\$|\1$fs$fs$fs\2|" \
+#         -e "s|$s\$||p" | \
+#     awk -F$fs "{
+#         gsub(/\t/,\"        \",\$1);
+#         gsub(\"name: \", \"\");
+#         if(NF>3){if(value!=\"\"){value = value \" \";}value = value  \$4;}
+#         else {
+#         if(match(\$1,/^&/)){anchor[substr(\$1,2)]=full_vn;getline};
+#         indent = length(\$1)/length(\"$i\");
+#         vname[indent] = \$2;
+#         value= \$3;
+#         for (i in vname) {if (i > indent) {delete vname[i]; idx[i]=0}}
+#         if(length(\$2)== 0){  vname[indent]= ++idx[indent] };
+#         vn=\"\"; for (i=0; i<indent; i++) { vn=(vn)(vname[i])(\"$separator\")}
+#         vn=\"$prefix\" vn;
+#         full_vn=vn vname[indent];
+#         if(vn==\"$prefix\")vn=\"$prefix$separator\";
+#         if(vn==\"_\")vn=\"__\";
+#         }
+#         assignment[full_vn]=value;
+#         if(!match(assignment[vn], full_vn))assignment[vn]=assignment[vn] \" \" full_vn;
+#         if(match(value,/^\*/)){
+#             ref=anchor[substr(value,2)];
+#             if(length(ref)==0){
+#             printf(\"%s=\\\"%s\\\"\n\", full_vn, value);
+#             } else {
+#             for(val in assignment){
+#                 if((length(ref)>0)&&index(val, ref)==1){
+#                     tmpval=assignment[val];
+#                     sub(ref,full_vn,val);
+#                 if(match(val,\"$separator\$\")){
+#                     gsub(ref,full_vn,tmpval);
+#                 } else if (length(tmpval) > 0) {
+#                     printf(\"%s=\\\"%s\\\"\n\", val, tmpval);
+#                 }
+#                 assignment[val]=tmpval;
+#                 }
+#             }
+#         }
+#     } else if (length(value) > 0) {
+#         printf(\"%s=\\\"%s\\\"\n\", full_vn, value);
+#     }
+#     }END{
+#         for(val in assignment){
+#             if(match(val,\"$separator\$\"))
+#                 printf(\"%s=\\\"%s\\\"\n\", val, assignment[val]);
+#         }
+#     }"
+
+# }
+# function cert_readConfig() {
+
+#     if [ -f "${config_file}" ]; then
+#         if [ ! -s "${config_file}" ]; then
+#             common_logger -e "File ${config_file} is empty"
+#             exit 1
+#         fi
+#         eval "$(cert_convertCRLFtoLF "${config_file}")"
+
+#         eval "indexer_node_names=( $(cert_parseYaml "${config_file}" | grep -E "nodes[_]+indexer[_]+[0-9]+=" | cut -d = -f 2 ) )"
+#         eval "server_node_names=( $(cert_parseYaml "${config_file}"  | grep -E "nodes[_]+server[_]+[0-9]+=" | cut -d = -f 2 ) )"
+#         eval "dashboard_node_names=( $(cert_parseYaml "${config_file}" | grep -E "nodes[_]+dashboard[_]+[0-9]+=" | cut -d = -f 2) )"
+#         eval "indexer_node_ips=( $(cert_parseYaml "${config_file}" | grep -E "nodes[_]+indexer[_]+[0-9]+[_]+ip=" | cut -d = -f 2) )"
+#         eval "server_node_ips=( $(cert_parseYaml "${config_file}"  | grep -E "nodes[_]+server[_]+[0-9]+[_]+ip=" | cut -d = -f 2) )"
+#         eval "dashboard_node_ips=( $(cert_parseYaml "${config_file}"  | grep -E "nodes[_]+dashboard[_]+[0-9]+[_]+ip=" | cut -d = -f 2 ) )"
+#         eval "server_node_types=( $(cert_parseYaml "${config_file}"  | grep -E "nodes[_]+server[_]+[0-9]+[_]+node_type=" | cut -d = -f 2 ) )"
+#         eval "number_server_ips=( $(cert_parseYaml "${config_file}" | grep -o -E 'nodes[_]+server[_]+[0-9]+[_]+ip' | sort -u | wc -l) )"
+
+#         for i in $(seq 1 "${number_server_ips}"); do
+#             nodes_server="nodes[_]+server[_]+${i}[_]+ip"
+#             eval "server_node_ip_$i=( $( cert_parseYaml "${config_file}" | grep -E "${nodes_server}" | sed '/\./!d' | cut -d = -f 2 | sed -r 's/\s+//g') )"
+#         done
+
+#         unique_names=($(echo "${indexer_node_names[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#         if [ "${#unique_names[@]}" -ne "${#indexer_node_names[@]}" ]; then 
+#             common_logger -e "Duplicated indexer node names."
+#             exit 1
+#         fi
+
+#         unique_ips=($(echo "${indexer_node_ips[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#         if [ "${#unique_ips[@]}" -ne "${#indexer_node_ips[@]}" ]; then 
+#             common_logger -e "Duplicated indexer node ips."
+#             exit 1
+#         fi
+
+#         unique_names=($(echo "${server_node_names[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#         if [ "${#unique_names[@]}" -ne "${#server_node_names[@]}" ]; then 
+#             common_logger -e "Duplicated Wazuh server node names."
+#             exit 1
+#         fi
+
+#         unique_ips=($(echo "${server_node_ips[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#         if [ "${#unique_ips[@]}" -ne "${#server_node_ips[@]}" ]; then 
+#             common_logger -e "Duplicated Wazuh server node ips."
+#             exit 1
+#         fi
+
+#         unique_names=($(echo "${dashboard_node_names[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#         if [ "${#unique_names[@]}" -ne "${#dashboard_node_names[@]}" ]; then
+#             common_logger -e "Duplicated dashboard node names."
+#             exit 1
+#         fi
+
+#         unique_ips=($(echo "${dashboard_node_ips[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#         if [ "${#unique_ips[@]}" -ne "${#dashboard_node_ips[@]}" ]; then
+#             common_logger -e "Duplicated dashboard node ips."
+#             exit 1
+#         fi
+
+#         for i in "${server_node_types[@]}"; do
+#             if ! echo "$i" | grep -ioq master && ! echo "$i" | grep -ioq worker; then
+#                 common_logger -e "Incorrect node_type $i must be master or worker"
+#                 exit 1
+#             fi
+#         done
+
+#         if [ "${#server_node_names[@]}" -le 1 ]; then
+#             if [ "${#server_node_types[@]}" -ne 0 ]; then
+#                 common_logger -e "The tag node_type can only be used with more than one Wazuh server."
+#                 exit 1
+#             fi
+#         elif [ "${#server_node_names[@]}" -gt "${#server_node_types[@]}" ]; then
+#             common_logger -e "The tag node_type needs to be specified for all Wazuh server nodes."
+#             exit 1
+#         elif [ "${#server_node_names[@]}" -lt "${#server_node_types[@]}" ]; then
+#             common_logger -e "Found extra node_type tags."
+#             exit 1
+#         elif [ "$(grep -io master <<< "${server_node_types[*]}" | wc -l)" -ne 1 ]; then
+#             common_logger -e "Wazuh cluster needs a single master node."
+#             exit 1
+#         elif [ "$(grep -io worker <<< "${server_node_types[*]}" | wc -l)" -ne $(( ${#server_node_types[@]} - 1 )) ]; then
+#             common_logger -e "Incorrect number of workers."
+#             exit 1
+#         fi
+
+#         if [ "${#dashboard_node_names[@]}" -ne "${#dashboard_node_ips[@]}" ]; then
+#             common_logger -e "Different number of dashboard node names and IPs."
+#             exit 1
+#         fi
+
+#     else
+#         common_logger -e "No configuration file found."
+#         exit 1
+#     fi
+
+# }
 function main(){
 
     umask 177
 
-
-    while [ -n "${1}" ]
-    do
-        case "${1}" in
-            "-a"|"--all-in-one")
-                AIO=1
-                shift 1
-                ;;
-            *)
-                echo "Unknow option: ${1}"
-                getHelp
-        esac
-    done
+    AIO=1
 
     cat /dev/null > "${logfile}"
 
@@ -1550,7 +1756,7 @@ function main(){
     dashboard_changePort "${http_port}"
 
     checks_ports "${wazuh_aio_ports[@]}"
-    
+
     common_logger "--- Wazuh indexer ---"
     indexer_install
     indexer_configure
