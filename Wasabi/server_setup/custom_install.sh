c7257b35a83eef8c4a68b74667d6712e1b514f11
mohamad khawam <khawammohamad99@gmail.com>
2023-12-07 12:19:04 -0500

    common configs?

--- Wasabi/server_setup/custom_install.sh
+++ Wasabi/server_setup/custom_install.sh
@@ -1128,7 +1128,7 @@ function indexer_configure() {
     eval "sed -i "s/-Xms1g/-Xms${ram}m/" /etc/wazuh-indexer/jvm.options ${debug}"
     eval "sed -i "s/-Xmx1g/-Xmx${ram}m/" /etc/wazuh-indexer/jvm.options ${debug}"
 
-    # eval "installCommon_getConfig indexer/indexer_all_in_one.yml /etc/wazuh-indexer/opensearch.yml ${debug}"
+    eval "installCommon_getConfig indexer/indexer_all_in_one.yml /etc/wazuh-indexer/opensearch.yml ${debug}"
 
     # if [ -n "${AIO}" ]; then
     #     eval "installCommon_getConfig indexer/indexer_all_in_one.yml /etc/wazuh-indexer/opensearch.yml ${debug}"
